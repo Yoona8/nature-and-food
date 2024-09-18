aac03485901e1616a4d4ae3b08f7dcce358e11f9
Yoona8 <elena243v@gmail.com>
2024-09-18 17:51:55 +0200

    feat: Add about section

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -6,8 +6,15 @@ export const Landing = () => {
     <>
       <section className="hero">
         <IconLeaves />
-        <h1 className="page-title hero__title">Amazing Vegan Lifestyle</h1>
+        <h1 className="page-title">Amazing Vegan Lifestyle</h1>
         <p className="hero__description">The best lifestyle ever. Fresh, good, healthy vegetarian recipes.</p>
+      </section>
+      <section className="about">
+        <h2 className="title-1">About</h2>
+        <p>
+          Raise your hand if you love falafel! I’ve gone back into the archives to highlight my all-time favorite
+          homemade falafel recipe. Making falafel at home can be tricky...
+        </p>
       </section>
     </>
   );

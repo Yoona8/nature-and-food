08bcfc70a97cc410d446660229e7e1be5ee20acf
Yoona8 <elena243v@gmail.com>
2024-10-14 08:29:54 +0200

    feat: Restructure styles

--- src/components/Recipes/Recipes.tsx
+++ src/components/Recipes/Recipes.tsx
@@ -1,18 +1,19 @@
-import './Recipes.css';
 import { IconLeaves } from '../../icons';
 import { RecipeCard } from '../RecipeCard/RecipeCard.tsx';
 import { mockedRecipes } from '../../__test__/__mocks__/recipes.mock.ts';
+import styles from './Recipes.module.css';
+import globalStyles from '../../shared.module.css';
 
 export const Recipes = () => {
   return (
     <>
-      <section className="hero">
+      <section className={styles.hero}>
         <IconLeaves />
-        <h1 className="page-title">Recipes</h1>
+        <h1 className={globalStyles.pageTitle}>Recipes</h1>
       </section>
-      <section className="recipes">
-        <h2 className="visually-hidden">Featured recipes</h2>
-        <ul className="recipes__cards">
+      <section className={styles.recipes}>
+        <h2 className={globalStyles.visuallyHidden}>Featured recipes</h2>
+        <ul className={styles.recipesCards}>
           {mockedRecipes.map(recipe => <li key={recipe.id}><RecipeCard {...recipe} /></li>)}
         </ul>
       </section>
